5760a47b9ebda18b998fbfb57266669031281f8f
elem577 <elem577@gmail.com>
2019-12-18 12:32:07 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,17 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Nantes",
-    dependencies: [
-        .package(
-            url: "https://github.com/Realm/SwiftLint",
-            from: "0.30.1"
-        ),
-    ],
     // Note: SPM requires 1 target to build the package
     targets: [
         .target(
             name: "Nantes",
-            dependencies: ["swiftlint"],
             path: "Source/Classes",
             sources: ["NantesLabel.swift"]
         )
